dced65359bca988b473643ef2fd49b701eaca1f7
Noornabi <noornabimd9891@gmail.com>
2025-11-17 22:36:14 +0600

    banner slider added

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import EmergencyIssues from '../../Components/EmergencyIssues/EmergencyIssues';
 
 const Home = () => {
     return (
         <div>
-            <h1>Hello world!</h1>
+            <EmergencyIssues/>
         </div>
     );
 };
